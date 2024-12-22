e580a3328afb0d0f4635a31b727ec40f68d2d27b
Vladislav Vasilev <vladen21@yandex.ru>
2024-12-22 22:44:13 +0300

    Add 3 Models

--- reltables/recipes/models.py
+++ reltables/recipes/models.py
@@ -1,4 +1,24 @@
 from django.db import models
-from
 
-class Recipe()
+
+class Recipe(models.Model):
+    name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=100
+    )
+    ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient')
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Ингредиент',
+        max_length=100
+    )
+    unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=10
+    )
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ManyToManyField()
